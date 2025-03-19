4bdd6959d9e6d334cac30022d7a34e185410e4b1
Kayke simao <kayke.py@gmail.com>
2025-03-18 21:26:57 -0300

    Challenge highScore completed

---
+++ exercism/1-highScore.ex
@@ -0,0 +1,9 @@
+defmodule HighScore do
+  def new(), do: %{}
+  @default_score 0
+  def add_player(scores, name, score \\ @default_score), do: Map.put(scores, name, score)
+  def remove_player(scores, name), do: Map.delete(scores, name)
+  def reset_score(scores, name), do: Map.update(scores, name, @default_score, &(&1 - &1))
+  def update_score(scores, name, score), do: scores |> Map.update(name, score, fn x ->  x + score end)
+  def get_players(scores), do: Map.keys(scores)
+end
