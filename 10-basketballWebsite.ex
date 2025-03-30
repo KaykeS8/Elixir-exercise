1289af38a0dc5f8c42a4f0818a4e5ac523e2a6a6
Kayke simao <kayke.py@gmail.com>
2025-03-30 11:29:09 -0300

    Running challenge

---
+++ 10-basketballWebsite.ex
@@ -0,0 +1,16 @@
+defmodule BasketballWebsite do
+
+  def extract_from_path(), do: nil
+  def extract_from_path(data, path) do
+  end
+end
+
+
+# data = %{
+#   "team_mascot" => %{
+#     "animal" => "bear",
+#     "actor" => %{
+#       "first_name" => "Noel"
+#     }
+#   }
+# }
