ecb7987c0bac09da7ed3a6314ae851be9ee98547
Kayke simao <kayke.py@gmail.com>
2025-04-19 12:51:47 -0300

    Challenge finished

--- exercise/lib/17-communityGarden.ex
+++ exercise/lib/17-communityGarden.ex
@@ -5,32 +5,35 @@ defmodule Plot do
 end
 
 defmodule CommunityGarden do
+  defstruct [counter_id: 0, plots: []]
+
+  defp initial_value, do: %CommunityGarden{counter_id: 0, plots: []}
   def start(opts \\ []) do
-    Agent.start(fn -> opts end)
+    Agent.start(&initial_value/0, opts)
   end
 
   def list_registrations(pid) do
-    Agent.get(pid, & &1)
+    Agent.get(pid, &(&1.plots))
   end
 
   def register(pid, register_to) do
-    Agent.update(pid, fn state ->
-      next_id =
-        case state do
-          [] -> 1
-          _ -> Enum.max_by(state, & &1.plot_id).plot_id + 1
-        end
-
-      plot = %Plot{plot_id: next_id, registered_to: register_to}
-      state ++ [plot]
-    end)
-
-    plot
+      Agent.get_and_update(pid, fn garden ->
+        plot = %Plot{plot_id: garden.counter_id + 1, registered_to: register_to}
+        {plot, %{counter_id: garden.counter_id + 1, plots: [plot | garden.plots]}}
+      end)
   end
 
   def release(pid, plot_id) do
+    Agent.update(pid, fn state ->
+      %{state | plots: state.plots |> Enum.filter(&(&1.plot_id != plot_id))}
+    end)
   end
 
   def get_registration(pid, plot_id) do
+    plot = list_registrations(pid) |> Enum.filter(&(&1.plot_id == plot_id)) |> List.first()
+    case plot do
+      nil -> {:not_found, "plot is unregistered"}
+      _ -> plot
+    end
   end
 end
